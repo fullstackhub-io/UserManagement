c601062f6d75f35c5391ed34bc1418d194a75656
Fullstack Hub <35650464+fullstackhub-io@users.noreply.github.com>
2020-10-18 17:25:39 -0400

    intial commit.
    intial commit.

---
+++ UserManagement.Domain/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace UserManagement.Domain.UnitOfWork
+{
+    using UserManagement.Domain.Repositories;
+    public interface IUnitOfWork
+    {
+        IUserRepository Users { get; }
+        void StartTransaction();
+        void Commit();
+    }
+}
