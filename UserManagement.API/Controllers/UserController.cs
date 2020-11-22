2d0e0df1131bf9c4a7ef411e1552a7811f115eb9
Fullstack Hub <35650464+fullstackhub-io@users.noreply.github.com>
2020-11-22 00:03:26 -0500

    Part 7
    Part 7

---
+++ UserManagement.API/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿namespace UserManagement.API.Controllers
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using UserManagement.Application.User.Commands;
+    using UserManagement.Application.User.Queries;
+    using UserManagement.Application.User.VM;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : BaseController
+    {
+        [HttpGet("[action]")]
+        public async Task<ActionResult<UserVM>> Get(int userID)
+        {
+            return await this.Mediator.Send(new GetSingleUserQuery { UserID = userID });
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult<UserVM>> GetAll()
+        {
+            return await this.Mediator.Send(new GetAllUserQuery());
+        }
+
+        [HttpPost("[action]")]
+        public async Task<ActionResult<int>> Post(AddUserCommand command)
+        {
+            return await this.Mediator.Send(command);
+        }
+
+        [HttpPut("[action]")]
+        public async Task<ActionResult<bool>> Put(UpdateUserCommand command)
+        {
+            return await this.Mediator.Send(command);
+        }
+
+        [HttpDelete("[action]")]
+        public async Task<ActionResult<bool>> Delete(int userID)
+        {
+            return await this.Mediator.Send(new DeleteUserCommand { UserID = userID });
+        }
+    }
+}
